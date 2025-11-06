bed3068c26f6651eb22c582b10ea33c34b762d82
Fahim <fahimmuntasir234@gmail.com>
2025-11-06 20:00:25 +0600

    Add blog section and implement scroll reveal animations

--- js/main.js
+++ js/main.js
@@ -43,3 +43,90 @@ item.addEventListener('click', () => {
 
   
 });
+
+// scroll reveal
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '60px',
+    duration: 3000,
+    delay: 600,
+    
+});
+
+//hero
+sr.reveal('.hero__text',{origin:'top'});
+sr.reveal('.hero__img');
+
+//stats
+sr.reveal('.stats__item',{
+    delay: 600,
+    distance: '100px',
+    interval: 100,
+    origin: 'top',
+});
+
+//services
+sr.reveal('.services');
+sr.reveal('.services__top');
+sr.reveal('.services__item',{
+    delay: 600,
+    distance: '100px',
+    interval: 100,
+    origin: 'bottom',
+});
+
+//appointment
+sr.reveal('.appointment__title');
+sr.reveal('.appointment__form');
+
+//testimonials
+sr.reveal('.testimonial');
+sr.reveal('.testimonial__constrainer');
+
+//team
+
+sr.reveal('.team__title');
+sr.reveal('.team__slider');
+
+//faq 
+sr.reveal('.faq__title');
+sr.reveal('.faq__item', {
+    delay: 600,
+    distance: '100px',
+    interval: 100,
+    origin: 'bottom',
+});
+
+//department
+sr.reveal('.departments__bg');
+sr.reveal('.departments__container');
+
+//blog
+sr.reveal('.blog__title');
+sr.reveal('.blog__post', {
+    delay: 600,
+    distance: '100px',
+    interval: 100,
+    origin: 'bottom',
+});
+
+//brands
+sr.reveal('.brands__logo', {
+    delay: 600,
+    distance: '100px',
+    interval: 100,
+    origin: 'bottom',
+});
+//newsletter
+sr.reveal('.newsletter');
+sr.reveal('.newsletter__container');
+
+sr.reveal('.footer__item', {
+    delay: 600,
+    distance: '100px',
+    interval: 100,
+    origin: 'bottom',
+});
+
+
+
